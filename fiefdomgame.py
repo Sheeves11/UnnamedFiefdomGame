572f15f7f7012300c8473085fa19a910c7af4c1c
Joshua <jad041@uark.edu>
2021-12-29 13:07:45 -0600

    Build(13) Garrison Soldier Command

--- fiefdomgame.py
+++ fiefdomgame.py
@@ -397,7 +397,7 @@ while (loop):
             withdrawNum = input('Enter the number of soldiers you would like to evenly distrubute among these ' + str(userFiefCount) + ' fiefs: ')
             time.sleep(1)
 
-            if !isinstance(withdrawNum, int):
+            if isinstance(withdrawNum, int) == False:
                 withdrawNum = '0'
 
             if int(withdrawNum) < 0:
