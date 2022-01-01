a2c8cc5f482870ee1c423ef347e9a8f1a990be57
Joshua <jad041@uark.edu>
2022-01-01 14:34:16 -0600

    Fixed instant map generation bug

--- worldmap.py
+++ worldmap.py
@@ -139,108 +139,107 @@ def PrintSurroundings(wMap, symb, posX, posY, freqM, freqP, freqF):
     global INSTANTLY_GENERATE
     #Create points that are surrounding our current pos
     os.system("clear")
+    print('posX: ' + str(posX) + ' posY: ' + str(posY))
+    try:
+        print('Current symbol: ' + symb)
+    except:
+        print('Current symbol: None')
+    try:
+        dN = wMap[posY - 1][posX]
+        print('There is a ' + dN + ' to the north!')
+    except:
+        dN = ' '
+        print('North is off the map!')
+    try:
+        dNE = wMap[posY - 1][posX + 1]
+        print('There is a ' + dNE + ' to the northeast!')
+    except:
+        dNE = ' '
+        print('Northeast is off the map!')
+    try:
+        dE = wMap[posY][posX + 1]
+        print('There is a ' + dE + ' to the east!')
+    except:
+        dE = ' '
+        print('East is off the map!')
+    try:
+        dSE = wMap[posY + 1][posX + 1]
+        print('There is a ' + dSE + ' to the southeast!')
+    except:
+        dSE = ' '
+        print('Southeast is off the map!')
+    try:
+        dS = wMap[posY + 1][posX]
+        print('There is a ' + dS + ' to the south!')
+    except:
+        dS = ' '
+        print('South is off the map!')
+    try:
+        dSW = wMap[posY + 1][posX - 1]
+        print('There is a ' + dSW + ' to the southwest!')
+    except:
+        dSW = ' '
+        print('Southwest is off the map!')
+    try:
+        dW = wMap[posY][posX - 1]
+        print('There is a ' + dW + ' to the west!')
+    except:
+        dW = ' '
+        print('West is off the map!')
+    try:
+        dNW = wMap[posY - 1][posX - 1]
+        print('There is a ' + dNW + ' to the northwest!')
+    except:
+        dNW = ' '
+        print('North is off the map!')
+
+    #Define a list using the surrounding symbols:
+    surroundings = [dN, dNE, dE, dSE, dS, dSW, dW, dNW]
+    
+    print('Surroundings: ') 
+    print(*surroundings)
+    #Define a list of weight totals for each:
+    weights = [0, 0, 0, 0, 0, 0, 0, 0]
+    index = 0
+
+    #Calculate the weight totals:
+    for i in surroundings:
+        if i == UNEXPLORED:
+            weights[index] = 0
+        elif i == EMPTY:
+            weights[index] = 0
+        elif i == WATER:
+            weights[index] = 10
+        elif i == RIVER:
+            weights[index] = 10
+        elif i == FOREST:
+            weights[index] = freqF * WEIGHT_INTENSITY
+        elif i == PLAINS:
+            weights[index] = freqP * WEIGHT_INTENSITY
+        elif i == MOUNTAIN:
+            weights[index] =  freqM * WEIGHT_INTENSITY
+        elif i == FIEF:
+            weights[index] = 0
+        elif i == STRONGHOLD:
+            weights[index] = 0
+        index = index + 1
+
+    print('Weights: ') 
+    print(*weights)
+    #Define a combined list of symbols and weights:
+    symbolTable = [(dN,weights[0]),(dNE,weights[1]),(dE,weights[2]),(dSE,weights[3]),(dS,weights[4]),(dSW,weights[5]),(dW,weights[6]),(dNW,weights[7]), (RANDOM,RANDOM_INTENSITY)]
+    print('Symbol Table: ') 
+    print(*symbolTable)
+    pointTable = []
+    for item, weight in symbolTable:
+        pointTable.extend([item]*weight)
+    newPoint = random.choice(pointTable)
+    print('New Point: ') 
+    print(*newPoint)
+    if newPoint == RANDOM:
+        newPoint = GetRandomPoint()
+
     if not INSTANTLY_GENERATE:
-        print('posX: ' + str(posX) + ' posY: ' + str(posY))
-        try:
-            print('Current symbol: ' + symb)
-        except:
-            print('Current symbol: None')
-        try:
-            dN = wMap[posY - 1][posX]
-            print('There is a ' + dN + ' to the north!')
-        except:
-            dN = ' '
-            print('North is off the map!')
-        try:
-            dNE = wMap[posY - 1][posX + 1]
-            print('There is a ' + dNE + ' to the northeast!')
-        except:
-            dNE = ' '
-            print('Northeast is off the map!')
-        try:
-            dE = wMap[posY][posX + 1]
-            print('There is a ' + dE + ' to the east!')
-        except:
-            dE = ' '
-            print('East is off the map!')
-        try:
-            dSE = wMap[posY + 1][posX + 1]
-            print('There is a ' + dSE + ' to the southeast!')
-        except:
-            dSE = ' '
-            print('Southeast is off the map!')
-        try:
-            dS = wMap[posY + 1][posX]
-            print('There is a ' + dS + ' to the south!')
-        except:
-            dS = ' '
-            print('South is off the map!')
-        try:
-            dSW = wMap[posY + 1][posX - 1]
-            print('There is a ' + dSW + ' to the southwest!')
-        except:
-            dSW = ' '
-            print('Southwest is off the map!')
-        try:
-            dW = wMap[posY][posX - 1]
-            print('There is a ' + dW + ' to the west!')
-        except:
-            dW = ' '
-            print('West is off the map!')
-        try:
-            dNW = wMap[posY - 1][posX - 1]
-            print('There is a ' + dNW + ' to the northwest!')
-        except:
-            dNW = ' '
-            print('North is off the map!')
-
-        #Define a list using the surrounding symbols:
-        surroundings = [dN, dNE, dE, dSE, dS, dSW, dW, dNW]
-    
-        print('Surroundings: ') 
-        print(*surroundings)
-        #Define a list of weight totals for each:
-        weights = [0, 0, 0, 0, 0, 0, 0, 0]
-        index = 0
-
-        #Calculate the weight totals:
-        for i in surroundings:
-            if i == UNEXPLORED:
-                weights[index] = 0
-            elif i == EMPTY:
-                weights[index] = 0
-            elif i == WATER:
-                weights[index] = 10
-            elif i == RIVER:
-                weights[index] = 10
-            elif i == FOREST:
-                weights[index] = freqF * WEIGHT_INTENSITY
-            elif i == PLAINS:
-                weights[index] = freqP * WEIGHT_INTENSITY
-            elif i == MOUNTAIN:
-                weights[index] =  freqM * WEIGHT_INTENSITY
-            elif i == FIEF:
-                weights[index] = 0
-            elif i == STRONGHOLD:
-                weights[index] = 0
-            index = index + 1
-
-        print('Weights: ') 
-        print(*weights)
-        #Define a combined list of symbols and weights:
-        symbolTable = [(dN,weights[0]),(dNE,weights[1]),(dE,weights[2]),(dSE,weights[3]),(dS,weights[4]),(dSW,weights[5]),(dW,weights[6]),(dNW,weights[7]), (RANDOM,RANDOM_INTENSITY)]
-        print('Symbol Table: ') 
-        print(*symbolTable)
-        pointTable = []
-        for item, weight in symbolTable:
-            pointTable.extend([item]*weight)
-        newPoint = random.choice(pointTable)
-        print('New Point: ') 
-        print(*newPoint)
-        if newPoint == RANDOM:
-            newPoint = GetRandomPoint()
-
-    
         GeneratePrintMap(wMap, posX, posY)
     
     if not AUTOMATED:
