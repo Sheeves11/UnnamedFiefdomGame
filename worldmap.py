b360fb2f0a20b2442ae5a9a51f87f5411bd1b256
Joshua <jad041@uark.edu>
2022-01-01 13:25:54 -0600

    Expanded on map generation functionality significantly. Created an option for manually sifting through the map during generation.

--- worldmap.py
+++ worldmap.py
@@ -10,7 +10,10 @@ from classes import *
 #Global variables
 MAP_WIDTH = 10
 MAP_HEIGHT = 10
-DEFAULT_WEIGHT = 1
+DEFAULT_WEIGHT = 10     #A common weight total
+WEIGHT_INTENSITY = 5    #Higher the number, the more focused the map will be
+RANDOM_INTENSITY = 20   #Higher the number, the more chaotic the map will be
+automated = False
 
 #Map Icons
 WATER = '~'
@@ -20,6 +23,9 @@ PLAINS = '#'
 FOREST = '^'
 FIEF = 'X'
 STRONGHOLD = 'H'
+EMPTY = ' '
+UNEXPLORED = '0'
+RANDOM = '*'
 
 #Manually inserted map-grid for visualizing. It is 20x8
 def ManualWorldMapSizeTest():
@@ -53,9 +59,9 @@ def ManualWorldMapSizeTest():
 #Fief locations will be determined later, but I'd like to have something that detects
 #the name of the fief and picks a location if it, say, has "Forest" in the name. More
 #on that later.
-def WorldMap(seed):
+def GenerateWorldMap(seed):
     os.system('clear')
-
+    automated = False
     #worldMap = [['0'] * MAP_WIDTH] * MAP_HEIGHT    #Change this later if we want to do small/medium/large map presets
     worldMap = [['0' for x in range(MAP_WIDTH)] for y in range(MAP_HEIGHT)]
 
@@ -88,18 +94,8 @@ def WorldMap(seed):
                 #print('Pos y: ' + str(y))
                 for x in range(MAP_WIDTH):
                     #print('Pos x: ' + str(x))
-
-                    #Prints whole map each time. Very taxing! Remove this later!
-                    for i in range(MAP_HEIGHT):
-                        for j in range(MAP_WIDTH):
-                            if i == y and j == x - 1:
-                                print(textColor.CYAN + worldMap[i][j] + textColor.RESET, end=" ")
-                            else:
-                                print(worldMap[i][j], end=" ")
-                        print('')
-                    time.sleep(0.3)
-
-                    temp = plotPoint(worldMap, symbol, x, y, freqMountain, freqPlains, freqForest)
+               
+                    temp = PrintSurroundings(worldMap, symbol, x, y, freqMountain, freqPlains, freqForest)
                     symbol = temp
 
                     worldMap[y][x] = symbol
@@ -123,7 +119,12 @@ def WorldMap(seed):
 
 #Iterates through the map given the map itself and a set of 
 #values to determine what to write in the next position.
-def plotPoint(wMap, symb, posX, posY, freqM, freqP, freqF):
+#ToDo I don't think symb is actually necessary, as it is just the last
+#thing drawn... which should be in the surrounding area.
+#This function could likely benefit from reaching two spaces out instead of just 1, but
+#that would make it far more complex. 
+def PrintSurroundings(wMap, symb, posX, posY, freqM, freqP, freqF):
+    
     #Create points that are surrounding our current pos
     os.system("clear")
     print('posX: ' + str(posX) + ' posY: ' + str(posY))
@@ -132,12 +133,17 @@ def plotPoint(wMap, symb, posX, posY, freqM, freqP, freqF):
     except:
         print('Current symbol: None')
     try:
-        dN = wMap[posY - 2][posX]
+        dN = wMap[posY - 1][posX]
         print('There is a ' + dN + ' to the north!')
     except:
         dN = ' '
         print('North is off the map!')
-    print('Current symbol: ' + symb)
+    try:
+        dNE = wMap[posY - 1][posX + 1]
+        print('There is a ' + dNE + ' to the northeast!')
+    except:
+        dNE = ' '
+        print('Northeast is off the map!')
     try:
         dE = wMap[posY][posX + 1]
         print('There is a ' + dE + ' to the east!')
@@ -145,21 +151,103 @@ def plotPoint(wMap, symb, posX, posY, freqM, freqP, freqF):
         dE = ' '
         print('East is off the map!')
     try:
-        dW = wMap[posY][posX - 2]
-        print('There is a ' + dW + ' to the west!')
+        dSE = wMap[posY + 1][posX + 1]
+        print('There is a ' + dSE + ' to the southeast!')
     except:
-        dW = ' '
-        print('West is off the map!')
+        dSE = ' '
+        print('Southeast is off the map!')
     try:
         dS = wMap[posY + 1][posX]
         print('There is a ' + dS + ' to the south!')
     except:
         dS = ' '
         print('South is off the map!')
+    try:
+        dSW = wMap[posY + 1][posX - 1]
+        print('There is a ' + dSW + ' to the southwest!')
+    except:
+        dSW = ' '
+        print('Southwest is off the map!')
+    try:
+        dW = wMap[posY][posX - 1]
+        print('There is a ' + dW + ' to the west!')
+    except:
+        dW = ' '
+        print('West is off the map!')
+    try:
+        dNW = wMap[posY - 1][posX - 1]
+        print('There is a ' + dNW + ' to the northwest!')
+    except:
+        dNW = ' '
+        print('North is off the map!')
 
-    newPoint = FIEF #temporary
+    #Define a list using the surrounding symbols:
+    surroundings = [dN, dNE, dE, dSE, dS, dSW, dW, dNW]
+    
+    print('Surroundings: ') print(surroundings)
+    #Define a list of weight totals for each:
+    weights = [0, 0, 0, 0, 0, 0, 0, 0]
+    
+
+    #Calculate the weight totals:
+    for i in surroundings:
+        if i == UNEXPLORED:
+            weights[i] = 0
+        elif i == EMPTY:
+            weights[i] = 0
+        elif i == WATER:
+            weights[i] = 10
+        elif i == RIVER:
+            weights[i] = 10
+        elif i == FOREST:
+            weights[i] = freqF * WEIGHT_INTENSITY
+        elif i == PLAINS:
+            weights[i] = freqP * WEIGHT_INTENSITY
+        elif i == MOUNTAIN:
+            weights[i] =  freqM * WEIGHT_INTENSITY
+        elif i == FIEF:
+            weights[i] = 0
+        elif i == STRONGHOLD:
+            weights[i] = 0
+
+    print('Weights: ') print(weights)
+    #Define a combined list of symbols and weights:
+    symbolTable = [(dN,weights[0]),(dNE,weights[1]),(dE,weights[2]),(dSE,weights[3]),(dS,weights[4]),(dSW,weights[5]),(dW,weights[6]),(dNW,weights[7]), (RANDOM,RANDOM_INTENSITY)]
+    print('Symbol Table: ') print(symbolTable)
+    pointTable = []
+    for item, weight in symbolTable:
+        pointTable.extend([item]*weight)
+    newPoint = random.choice(pointTable)
+    print('New Point: ') print(newPoint)
+    if newPoint == RANDOM:
+        newPoint = GetRandomPoint()
+
+    PrintMap(wMap, posX, posY)
+    
+    if not automated:
+        userInput=input('Continue with manual input: press enter or input (n)')
+
+        if userInput == n
+            automated = True
+
     return newPoint
 
+def GetRandomPoint():
+    symbolTable = [(WATER, DEFAULT_WEIGHT),(FOREST, DEFAULT_WEIGHT), (PLAINS, DEFAULT_WEIGHT), (MOUNTAIN, DEFAULT_WEIGHT)]
+    pointTable = []
+    for item, weight in symbolTable:
+        pointTable.extend([item]*weight)
+    return random.choice(pointTable)
+
+def PrintMap(wMap, posX, posY):
+    for i in range(MAP_HEIGHT):
+        for j in range(MAP_WIDTH):
+            if i == posY and j == posX - 1:
+                print(textColor.CYAN + wMap[i][j] + textColor.RESET, end=" ")
+            else:
+                print(wMap[i][j], end=" ")
+        print('')
+    time.sleep(0.3)
 
 
 #To Do: Write this function
