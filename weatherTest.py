ba93762c7e4becbeeff9863711d8c32a3a82244a
Sheeves11 <mikequain11@gmail.com>
2022-01-13 11:53:48 -0600

    progress on the weather system

--- weatherTest.py
+++ weatherTest.py
@@ -54,6 +54,64 @@ if serverMap.worldMap[int(weatherStronghold.yCoordinate)][int(weatherStronghold.
 localTemp = tempMod + baselineTemp + weatherSystemMod
 
 
+def getLocalTemp(y, x, baselineTemp, weatherSystemMod):
+
+    #get map symbol for current location
+#    symbol = serverMap.worldMap[y][x]
+    symbol = 'W'
+
+    tempMod = 0
+    if serverMap.worldMap[int(y)][int(x)] == '^':
+        tempMod = -5
+
+    if serverMap.worldMap[int(y)][int(x)] == 'M':
+        tempMod = -20
+
+    if serverMap.worldMap[int(y)][int(x)] == '~':
+        tempMod = 0
+
+    if serverMap.worldMap[int(y)][int(x)] == '/':
+        tempMod = 3
+
+    if serverMap.worldMap[int(y)][int(x)] == '|':
+        tempMod = 3
+
+    if serverMap.worldMap[int(y)][int(x)] == '\\':
+        tempMod = -1
+
+    realTemp = tempMod + baselineTemp + weatherSystemMod
+
+    #assign colors based on temp
+    if realTemp > 70 and realTemp < 90:
+        print(textColor.RED, end = '')
+        print(str(symbol), end = '')
+        print(textColor.RESET, end = '  ')
+
+    if realTemp > 60 and realTemp <= 70:
+        print(textColor.ORANGE, end = '')
+        print(str(symbol), end = '')
+        print(textColor.RESET, end = '  ')
+
+    if realTemp > 50 and realTemp <= 60:
+        print(textColor.YELLOW, end = '')
+        print(str(symbol), end = '')
+        print(textColor.RESET, end = '  ')
+
+    if realTemp > 40 and realTemp <= 50:
+        print(textColor.GREEN, end = '')
+        print(str(symbol), end = '')
+        print(textColor.RESET, end = '  ')
+
+    if realTemp > 30 and realTemp <= 40:
+        print(textColor.BLUE, end = '')
+        print(str(symbol), end = '')    
+        print(textColor.RESET, end = '  ')
+
+    
+#    print(str(realTemp), end = ' ')    
+
+
+
 print('------------------------------Weather Calculations-------------------------------------------')
 print('Baseline (Global) Temp: ' + str(baselineTemp))
 print('Temp Biome Modifier: ' + str(tempMod))
@@ -69,14 +127,32 @@ print('\n')
 #    print(str(serverMap.worldMap[i][5]))
 #    print(' ')
 
- 
-for i in range(MAP_HEIGHT):             #Iterate through each row of the map
-    for j in range(MAP_WIDTH):          #Iterate through each symbol in the row
-        if j == 0 and i != 0:
-            print(str(serverMap.worldMap[i][j]))
-        else:
-            print(str(serverMap.worldMap[i][j]), end = " ")
-print('')
+for i in range(MAP_HEIGHT):
+        for j in range(MAP_WIDTH):
+            symbol = serverMap.worldMap[i][j]
+            if j == 0:
+                print('    ')
+                getLocalTemp(i, j, baselineTemp, weatherSystemMod)
+
+            else:
+                getLocalTemp(i,j, baselineTemp, weatherSystemMod)
+               
+        print('')
+
+
+
+
+
+
+
+
+#for i in range(MAP_HEIGHT):             #Iterate through each row of the map
+#    for j in range(MAP_WIDTH):          #Iterate through each symbol in the row
+#        if j == 0 and i != 0:
+#            print(str(serverMap.worldMap[i][j]))
+#        else:
+#            print(str(serverMap.worldMap[i][j]), end = " ")
+#print('')
 
 print('\n\n')
 print('---------------------------------------------------------------------------------------------')
