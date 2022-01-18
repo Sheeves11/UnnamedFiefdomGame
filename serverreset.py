eb5b350dec6207e8e2bfbc54d61e6490d1caee6e
Sheeves11 <mikequain11@gmail.com>
2022-01-18 09:33:40 -0600

    this program now resets or initializes all server data files

--- serverreset.py
+++ serverreset.py
@@ -4,64 +4,95 @@ import random
 
 '''
 
-Welcome to the Unnamed Fiefdom Game!
+This program deletes and initializes all data files
+for the Unnamed Fiefdom Game.
 
-This game was designed and written by
-Mike Quain of the University of Arkansas
-
-More info can be found at
-github.com/Sheeves11/UnnamedFiefdomGame
+More info at github.com/Sheeves11/unnamedfiefdomgame
 
 '''
 
 #initial screen clear
 os.system("clear")
 
-answer = input('Would you like to wipe the server and all user data? (y/n)\n')
+answer = input('Would you like to wipe the server and all user data? (y/n): ')
 
 if answer == 'y':
-    print('Deleting all text files...\n\n')
+    print('\nDeleting all text files...\n\n')
 
+    #Delete Fiefdoms
     dir_name = "fiefs/"
     test = os.listdir(dir_name)
     for item in test:
         if item.endswith(".txt"):
             os.remove(os.path.join(dir_name, item))
+    print('Fiefdoms Deleted')
 
+    #Delete Users
     dir_name = "users/"
     test = os.listdir(dir_name)
     for item in test:
         if item.endswith(".txt"):
             os.remove(os.path.join(dir_name, item))
+    print('User Account Files Deleted')
 
+    #Delete Strongholds
     dir_name = "strongholds/"
     test = os.listdir(dir_name)
     for item in test:
         if item.endswith(".txt"):
             os.remove(os.path.join(dir_name, item))
+    print('User Strongholds Deleted')
 
-    print('Delete Complete\n\n')
-    throwaway = input('Press Enter To Contiue:\n')
-    pass
+    #Rewrite/Initialize settings.txt
+    try:
+        with open('settings.txt', 'r+') as settingsFile:
+            print('Opened settings.txt')
 
-try:
-    with open('settings.txt', 'r+') as settingsFile:
-        print('Opened settings.txt')
-        line = settingsFile.readline().strip()
-        if line.endswith('yes'):
-            print('Settings.txt ends with yes.')
-            settingsFile.seek(0)
-            print('Attempting to write over line')
+            print('Attempting to write over file')
+            settingsFile.truncate() 
             settingsFile.write('Map Initialized: no')
-            print('Wrote over the line!')
+            print('Server Settings Initialized')
+
             settingsFile.close()
-        else:
-            # print('Settings did not end in no!')
-            settingsFile.close()
-except:
-    print('Error, something wrong with settings.txt!')
+    except:
+        print('settings.txt rewrite did not succeed')
+
+    #Truncate serverMap.txt
+    try:
+        with open('map/serverMap.txt', 'r+') as mapFile:
+            print('Opened settings.txt')
+
+            mapFile.truncate() 
+            mapFile.close()
+            print('Server Map Truncated')
+    except:
+        print('serverMap.txt rewrite did not succeed')
+
+    #Truncate serverMarket.txt
+    try:
+        with open('market/serverMarket.txt', 'r+') as marketFile:
+            print('Opened marketFile.txt')
+
+            marketFile.truncate() 
+            marketFile.close()
+            print('Market File Truncated')
+    except:
+        print('marketFile.txt rewrite did not succeed')
+
+    #Truncate chatlog.log
+    try:
+        with open('chatlog.log', 'r+') as chatFile:
+            print('Opened marketFile.txt')
+
+            chatFile.truncate() 
+            chatFile.close()
+            print('Chat Log Truncated')
+    except:
+        print('Chat Log rewrite did not succeed')
+
+    print('\nDelete Complete. Your server is now ready to go!\n')
+    throwaway = input('Press Enter To Contiue:\n')
 
 if answer != 'y':
     print('Aborting Reset Process')
     throwaway = input('Press Enter To Contiue:\n')
-    pass
